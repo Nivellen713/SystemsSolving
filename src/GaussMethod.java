0e201cabf274174b469ca717c3b4571ce1d544b1
Tony <czvetkov.1990@bk.ru>
2020-10-10 09:18:19 +0300

    Update Gauss method's interface

--- src/GaussMethod.java
+++ src/GaussMethod.java
@@ -5,66 +5,53 @@ public class GaussMethod {
 
         General general = new General();
         Scanner scanner = new Scanner(System.in);
-
-        double[][] testMatrix = {{1, -1, 3, 1}, {4, -1, 5, 4}, {2, -2, 4, 1}, {1, -4, 5, -1}}; // коэффициенты при неизвестных
-        double[][] rememberMatrix = new double[4][4];
-        double[] dopMatrix = {5, 4, 6, 3};   // свободные члены СЛАУ
-
         double det;
-        double element;
-        int n = 3;
-
-        double[] x = new double[dopMatrix.length];
+        int amountOfUnknowns;
         double d, s;
 
         //------------------------- Вводим матрицу -------------------------//
 
-//        System.out.println("Введите коэффициенты при x");
-//        for (int i = 0; i < 3; i++){
-//            testMatrix[i][0] = scanner.nextInt();
-//            rememberMatrix[i][0] = testMatrix[i][0];
-//        }
-//
-//        System.out.println("Введите коэффициенты при y");
-//        for (int i = 0; i < 3; i++){
-//            testMatrix[i][1] = scanner.nextInt();
-//            rememberMatrix[i][1] = testMatrix[i][1];
-//        }
-//
-//        System.out.println("Введите коэффициенты при z");
-//        for (int i = 0; i < 3; i++){
-//            testMatrix[i][2] = scanner.nextInt();
-//            rememberMatrix[i][2] = testMatrix[i][2];
-//        }
-//
-//        System.out.println("Введите свободные члены");
-//        for (int i = 0; i < 3; i++){
-//            dopMatrix[i] = scanner.nextInt();
-//        }
+        System.out.println("Введите количество неизвестных: ");
+        amountOfUnknowns = scanner.nextInt();
+        double[][] testMatrix = new double[amountOfUnknowns][amountOfUnknowns];
+        double[] dopMatrix = new double[amountOfUnknowns];
+        double[] x = new double[amountOfUnknowns];
 
-        System.out.println('\n' + "Определитель вашей матрицы, сир: ");
+        for (int j = 1; j <= amountOfUnknowns ; j++) {
+            System.out.println("Введите " + j + " строку: ");
+            for (int i = 0; i < amountOfUnknowns; i++){
+                testMatrix[i][j - 1] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Введите свободные члены: ");
+        for (int i = 0; i < amountOfUnknowns; i++){
+            dopMatrix[i] = scanner.nextInt();
+        }
+
+        System.out.println('\n' + "Определитель матрицы: ");
         det = general.determinant(testMatrix);
         System.out.println("determinant = " + det);
 
         //------------- ПРЯМОЙ ХОД -------------//
-        for (int k = 0; k <= n; k++){
-            for (int j = k + 1; j <= n; j++){
-                d = testMatrix[j][k] / testMatrix[k][k];    // Умножение k-й строки на число
+        for (int k = 0; k <= amountOfUnknowns - 1; k++){
+            for (int j = k + 1; j <= amountOfUnknowns - 1; j++){
+                d = testMatrix[j][k] / testMatrix[k][k];    // Умножение k-й строки на число. (1)
                 // Вычитание k-й строки из j-й строки
-                for (int i = k; i <= n; i++){
-                    testMatrix[j][i] -= d * testMatrix[k][i];
+                for (int i = k; i <= amountOfUnknowns - 1; i++){
+                    testMatrix[j][i] -= d * testMatrix[k][i];   // (2)
                 }
-                dopMatrix[j] -= d * dopMatrix[k];
+                dopMatrix[j] -= d * dopMatrix[k];   // (3)
             }
         }
             //------------- ОБРАТНЫЙ ХОД -------------//
-            for (int k = n; k >= 0; k--){
+            for (int k = amountOfUnknowns - 1; k >= 0; k--){
                 d = 0;
-                for (int j = k + 1; j <= n; j++){
-                    s = testMatrix[k][j] * x[j];    // Вычисление неизвестных
-                    d += s;
+                for (int j = k + 1; j <= amountOfUnknowns - 1; j++){
+                    s = testMatrix[k][j] * x[j];    // Вычисление неизвестных. (4)
+                    d += s; // (4)
                 }
-                x[k] = (dopMatrix[k] - d) / testMatrix[k][k];
+                x[k] = (dopMatrix[k] - d) / testMatrix[k][k];   // (4)
         }
         System.out.println("\nРешение:");
         for (double v : x) {
