eac5086ec02231651467444f2949cf9f768e4e9f
Anton Tsvetkov <55020949+Nivellen713@users.noreply.github.com>
2020-10-15 09:07:56 +0300

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -8,7 +8,6 @@ public class Runner {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите порядок матрицы: ");
         int n = scanner.nextInt();
-//        int n = 4;
         try {
             Matrix matrix = new Matrix(n, n);
             LinearMatrix dopMatrix = new LinearMatrix(n);
@@ -18,32 +17,7 @@ public class Runner {
             System.out.println("Your matrix: " + matrix);
             System.out.println("Dop matrix: " + dopMatrix);
 
-//            matrix.setElement(0, 0, 1);
-//            matrix.setElement(0, 1, -1);
-//            matrix.setElement(0, 2, 3);
-//            matrix.setElement(0, 3, 1);
-//
-//            matrix.setElement(1, 0, 4);
-//            matrix.setElement(1, 1, -1);
-//            matrix.setElement(1, 2, 5);
-//            matrix.setElement(1, 3, 4);
-//
-//            matrix.setElement(2, 0, 2);
-//            matrix.setElement(2, 1, -2);
-//            matrix.setElement(2, 2, 4);
-//            matrix.setElement(2, 3, 1);
-//
-//            matrix.setElement(3, 0, 1);
-//            matrix.setElement(3, 1, -4);
-//            matrix.setElement(3, 2, 5);
-//            matrix.setElement(3, 3, -1);
-//
-//            dopMatrix.setElement(0, 5);
-//            dopMatrix.setElement(1, 4);
-//            dopMatrix.setElement(2, 6);
-//            dopMatrix.setElement(3, 3);
-
-            System.out.println("Choose a solution method.\nCramer of Gauss");
+            System.out.println("\nChoose a solution method.\nCramer of Gauss");
             String solutionMethod = scanner.next();
             solutionMethod = solutionMethod.trim().toLowerCase();
 
